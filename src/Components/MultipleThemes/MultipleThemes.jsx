da271964afca65cdf545bbba005c111f845f30ab
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-12-08 15:12:57 +0100

    add functionality edit to themes

--- src/Components/MultipleThemes/MultipleThemes.jsx
+++ src/Components/MultipleThemes/MultipleThemes.jsx
@@ -1,65 +1,143 @@
 import { uid } from "uid";
-import "./MultipleThemes.css"
+import "./MultipleThemes.css";
 import useLocalStorageState from "use-local-storage-state";
+import { initialThemes } from "../../lib/themes";
+import { useState } from "react";
 
-export default function MultipleThemes({ 
-    themes ,
-    selectedThemeId, 
-    handleThemeSelect,
-    handleAddTheme,
- }) {
+export default function MultipleThemes({
+  themes,
+  selectedThemeId,
+  handleThemeSelect,
+  handleAddTheme,
+  onEditTheme,
+}) {
+  const [newThemeName, setNewThemeName] = useLocalStorageState("newThemeName", {
+    defaultValue: "",
+  });
+  const currentTheme = themes.find((theme) => theme.id === selectedThemeId);
+  const themeName = currentTheme?.name || "Unknown Theme";
+  const [isAdding, setIsAdding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-    const [newThemeName, setNewThemeName] = useLocalStorageState("newThemeName", {defaultValue: ""});
+  function handleInputValue(event) {
+    setNewThemeName(event.target.value);
 
-    function handleInputValue(event) {
-        setNewThemeName(event.target.value);
+  }
+
+  function isDuplicateName(newThemeName) {
+    const newName = newThemeName.toLowerCase();
+    return themes.some((theme) => theme.name.toLowerCase() === newName);
+  }
+
+  function addTheme() {
+    setIsAdding(true);
+  }
+  function cancelAddTheme() {
+    setIsAdding(false);
+  }
+
+  function handleNewTheme(newThemeName) {
+    if (newThemeName.trim() === "") {
+      alert("Please add a Theme name");
+    } else if (isDuplicateName(newThemeName)) {
+      alert("This Theme name already exists");
+    } else {
+      const newTheme = { id: uid(), name: newThemeName, colors: [] };
+      handleAddTheme(newTheme);
+      setNewThemeName("");
+      setIsAdding(false);
     }
+  }
 
-    function isDuplicateName(newThemeName) {
-        const newName = newThemeName.toLowerCase();
-        return themes.some((theme) => theme.name.toLowerCase() === newName);
-    }
-    function handleNewTheme(newThemeName) {
+  function cancelEditTheme() {
+    setIsEditing(false);
+  }
 
-        if (newThemeName.trim() === "") {
-            alert("Please add a Theme name");
-        } else if (isDuplicateName(newThemeName))  {
-            alert("This Theme name already exists"); 
-               
-            }  else { 
-                const newTheme = {id: uid(), name: newThemeName, colors: []};
-                console.log("newTheme: ", newTheme);
-                handleAddTheme(newTheme);
-                setNewThemeName(newThemeName);
-            } 
-    }
+  function handleEditTheme() {
+    setIsEditing(true);
+  }
 
+  function handleRenameTheme(newThemeName) {
+    if (newThemeName.trim() === "") {
+        alert("Please add a Theme name");
+      } else if (isDuplicateName(newThemeName)) {
+        alert("This Theme name already exists");
+      } else {
+    onEditTheme(newThemeName, selectedThemeId);
+    setIsEditing(false);}
+  }
 
+  function handeleDeleteTheme() {}
 
-
-
-
-return(
-    <div className="theme-container" >
-    <h2>Chose your Theme or create a new one</h2>
-    <label htmlFor="theme" >Your Theme: </label>
-    <select name="theme" id="theme" value={selectedThemeId} 
-      onChange={(e) => handleThemeSelect(e.target.value) }>
+  return (
+    <div className="theme-container">
+      <h2>Chose your Theme or create a new one</h2>
+      <label htmlFor="theme">Your Theme: </label>
+      <select
+        name="theme"
+        id="theme"
+        value={selectedThemeId}
+        onChange={(e) => handleThemeSelect(e.target.value)}
+      >
         {themes.map((theme) => (
-          <option key={theme.id} value={theme.id} >
+          <option key={theme.id} value={theme.id}>
             {theme.name}
           </option>
         ))}
-    </select> <br />
-    <div className="newTheme" >
-    <button onClick={() => handleNewTheme(newThemeName)} >Add a new Theme</button>
-    <label htmlFor="newTheme">Your new Theme name:</label>
-    <input type="text" id="newTheme" value={newThemeName} 
-            onChange={handleInputValue} 
-            placeholder="Your new Themename"/>
+      </select>{" "}
+      <br />
+      <div className="newTheme">
+        <button onClick={addTheme}>Add a new Theme</button>
+        <button
+          onClick={() => handleEditTheme}
+          disabled={selectedThemeId === initialThemes[0].id}
+        >
+          Edit the Theme
+        </button>
+        <button
+          onClick={() => handeleDeleteTheme}
+          disabled={selectedThemeId === initialThemes[0].id}
+        >
+          Delete the Theme
+        </button>
+
+        {isAdding ? (
+          <>
+            <label htmlFor="newTheme">Your new Theme name:</label>
+            <input
+              type="text"
+              id="newTheme"
+              value={newThemeName}
+              onChange={handleInputValue}
+              placeholder="Your new Themename"
+            />
+            <div>
+              <button onClick={cancelAddTheme}>Cancel</button>
+              <button onClick={() => handleNewTheme(newThemeName)}>Add</button>
             </div>
+          </>
+        ) : isEditing ? (
+          <>
+            <p>The current Theme name is: {themeName}</p>
+            <label htmlFor="newTheme">Your new Theme name:</label>
+            <input
+              type="text"
+              id="newTheme"
+              value={newThemeName}
+              onChange={handleInputValue}
+              placeholder="Your new Themename"
+            />
+            <div>
+              <button onClick={cancelEditTheme}>Cancel</button>
+              <button onClick={() => handleRenameTheme(newThemeName)}>
+                Rename
+              </button>
+            </div>
+          </>
+        ) : (
+          <p></p>
+        )}
+      </div>
     </div>
-
-)
-
+  );
 }
